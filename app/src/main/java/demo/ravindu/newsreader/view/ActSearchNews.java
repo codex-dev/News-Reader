281ecfad0b64552e7dd2e065e23983424cce0f9e
Ravindu Fernando <ravinduf@lolctech.com>
2021-12-31 08:17:14 +0530

    initial commit

---
+++ app/src/main/java/demo/ravindu/newsreader/view/ActSearchNews.java
@@ -0,0 +1,16 @@
+package demo.ravindu.newsreader.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import demo.ravindu.newsreader.R;
+
+public class ActSearchNews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_search_news);
+    }
+}
